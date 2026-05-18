8701593692113ec5ae4f5104b00733ea7abb2616
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-05-18 09:39:30 -0300

    Update next.config.mjs

--- v2/next.config.mjs
+++ v2/next.config.mjs
@@ -8,14 +8,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        // Site legado servido na raiz
-        { source: '/', destination: '/index.html' },
-      ],
-    }
-  },
 }
 
 export default nextConfig
