f6b915cef227601630a1fd89516a47252db1d63c
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-05-18 09:34:18 -0300

    Update next.config.mjs

--- v2/next.config.mjs
+++ v2/next.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/v2',
+  assetPrefix: '/v2',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // Site legado servido na raiz
+        { source: '/', destination: '/index.html' },
+      ],
+    }
   },
 }
 
